b3db8455a0956d8a07045907617b3d262c128d82
Mr3zee <284627@niuitmo.ru>
2021-06-03 23:36:21 +0300

    folk done

--- src/main/java/algo/era/FolkloreEra.java
+++ src/main/java/algo/era/FolkloreEra.java
@@ -15,11 +15,41 @@ public class FolkloreEra implements TaylorSwift {
 
     @Override
     public double[] noBodyNoCrime(double[] v) {
-        return new double[0];
+        return new double[] {
+                2 * (v[0] + 10 * v[1]),
+                4 * (10 * Math.pow(v[1] - v[3], 3) + 5 * (v[0] + 10 * v[1]) + Math.pow(v[1] - 2 * v[2], 3)),
+                10 * (v[2] - v[3]) - 8 * Math.pow(v[1] - 2 * v[2], 3),
+                10 * (4 * Math.pow(v[3] - v[1], 3) + v[3] - v[2])
+        };
     }
 
     @Override
     public double[][] coneyIsland(double[] v) {
-        return new double[0][];
+        return new double[][] {
+                {
+                    2,
+                    20,
+                    0,
+                    0
+                },
+                {
+                    20,
+                    4 * (30 * Math.pow(v[1] - v[3], 2) + 50 + 3 * Math.pow(v[1] - 2 * v[2], 2)),
+                    -24 * Math.pow(v[1] - 2 * v[2], 2),
+                    -40 * Math.pow(v[1] - v[3], 2)
+                },
+                {
+                    0,
+                    -24 * Math.pow(v[1] - 2 * v[3], 2),
+                    10 + 48 * Math.pow(v[1] - 2 * v[3], 2),
+                    -10
+                },
+                {
+                    0,
+                    120 * Math.pow(v[3] - v[1], 2),
+                    -10,
+                    10 + 12 * Math.pow(v[3] - v[1], 2)
+                }
+        };
     }
 }
